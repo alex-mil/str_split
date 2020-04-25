dabc598f59a94d77730d9de414d752d20a28f739
Alexander Milikovski <alexm33@gmail.com>
2020-04-25 20:28:44 +0300

    feat: split over generic pattern that implements custom trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-//!
-// #![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
-
-#[derive(Debug)]
-pub struct StrSplit<'haystack, 'delimeter> {
-    remainder: Option<&'haystack str>,
-    delimiter: &'delimeter str,
+pub trait Delimiter {
+    // Option<(start, end)>
+    fn find_next(&self, slice: &str) -> Option<(usize, usize)>;
 }
 
-impl<'haystack, 'delimeter> StrSplit<'haystack, 'delimeter> {
-    pub fn new(haystack: &'haystack str, delimiter: &'delimeter str) -> Self {
+#[derive(Debug)]
+pub struct StrSplit<'haystack, D> {
+    remainder: Option<&'haystack str>,
+    delimiter: D,
+}
+
+impl<'haystack, D> StrSplit<'haystack, D> {
+    pub fn new(haystack: &'haystack str, delimiter: D) -> Self {
         StrSplit {
             remainder: Some(haystack),
             delimiter,
@@ -16,7 +18,10 @@ impl<'haystack, 'delimeter> StrSplit<'haystack, 'delimeter> {
     }
 }
 
-impl<'haystack> Iterator for StrSplit<'haystack, '_> {
+impl<'haystack, D> Iterator for StrSplit<'haystack, D>
+where
+    D: Delimiter,
+{
     type Item = &'haystack str;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -24,9 +29,9 @@ impl<'haystack> Iterator for StrSplit<'haystack, '_> {
         // "?" operator returns something inside Option<T> or None
         let reminder = self.remainder.as_mut()?;
 
-        if let Some(idx) = reminder.find(self.delimiter) {
-            let prefix = &reminder[..idx];
-            *reminder = &reminder[(idx + self.delimiter.len())..];
+        if let Some((delim_start, delim_end)) = self.delimiter.find_next(reminder) {
+            let prefix = &reminder[..delim_start];
+            *reminder = &reminder[delim_end..];
             Some(prefix)
         } else {
             self.remainder.take()
@@ -34,9 +39,26 @@ impl<'haystack> Iterator for StrSplit<'haystack, '_> {
     }
 }
 
+impl Delimiter for &str {
+    // Option<(start, end)>
+    fn find_next(&self, slice: &str) -> Option<(usize, usize)> {
+        slice.find(self).map(|idx| (idx, idx + self.len()))
+    }
+}
+
+impl Delimiter for char {
+    // Option<(start, end)>
+    fn find_next(&self, slice: &str) -> Option<(usize, usize)> {
+        slice
+            .char_indices()
+            .find(|(_idx, ch)| ch == self)
+            .map(|(idx, _ch)| (idx, idx + self.len_utf8()))
+    }
+}
+
 #[allow(dead_code)]
 pub fn prefix_until_char(s: &str, ch: char) -> &str {
-    StrSplit::new(s, &ch.to_string())
+    StrSplit::new(s, ch)
         .next()
         .expect("StrSplit always gives at least one result")
 }
